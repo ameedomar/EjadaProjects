fd145f8cd9dcd6f295609682fdff6df4fa62c925
Ameed Omar <ameed_201010@hotmail.com>
2023-04-11 11:31:31 +0200

    10.04.2023 Task

---
+++ Task10.04/script.js
@@ -0,0 +1,21 @@
+function calculateCount() {
+  if (document.getElementById("sentence").value.length > 0) {
+    var sentence = document.getElementById("sentence").value;
+    var words = sentence
+      .trim()
+      .split(" ")
+      .filter((item) => item.trim().length > 0);
+
+    var countOfCharacters = 0;
+
+    for (var i = 0; i < words.length; i++) {
+      countOfCharacters += words[i].length;
+    }
+
+    document.getElementById("countW").innerHTML = words.length;
+    document.getElementById("countC").innerHTML = countOfCharacters;
+  } else {
+    document.getElementById("countW").innerHTML = 0;
+    document.getElementById("countC").innerHTML = 0;
+  }
+}
